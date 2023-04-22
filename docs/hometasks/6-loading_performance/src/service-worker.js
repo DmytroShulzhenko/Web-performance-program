16e6ffbcb535ec5628961c5e99c2f3d6ded2ea4e
Dmytro Shulzhenko <dmytro_shulzhenko@epam.com>
2023-04-22 15:11:17 +0300

    [DSh]: made tasks for loading_performance

--- docs/hometasks/6-loading_performance/src/service-worker.js
+++ docs/hometasks/6-loading_performance/src/service-worker.js
@@ -1,1 +1,41 @@
 // Here make implementation of service worker
+self.addEventListener('install', function(e) {
+  console.log('Install Event:', e);
+
+  e.waitUntil(
+    caches.open('mysite-static-v3').then(
+      function (cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/index.js',
+          '/styles/style.scss',
+        ]);
+      }
+    ),
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(res => {
+              return caches.open('dynamic')
+                .then(cache => {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            });
+        }
+      })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('Activate Event:', e);
+});
