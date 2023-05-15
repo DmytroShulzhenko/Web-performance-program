1af3c72a4ef8289db80f5020ee1a31e13938e570
DmytroShulzhenko <39739080+DmytroShulzhenko@users.noreply.github.com>
2023-05-15 11:40:42 +0300

    Origin/feature/home task   6 loading performance (#8)
    * [DSh]: made tasks for loading_performance
    
    * [DSh]: made tasks for loading_performance
    
    ---------
    
    Co-authored-by: Dmytro Shulzhenko <dmytro_shulzhenko@epam.com>

--- docs/hometasks/6-loading_performance/webpack.config.js
+++ docs/hometasks/6-loading_performance/webpack.config.js
@@ -1,42 +1,55 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const miniCss = require('mini-css-extract-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-            {
-                test: /\.(scss)$/,
-                use: [
-                    miniCss.loader,
-                    "css-loader",
-                    "sass-loader",
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                loader: 'file-loader',
-                options: {
-                    outputPath: 'images',
-                },
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        }),
-        new miniCss({
-            filename: 'style.css',
-        }),
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, './build'),
+    publicPath: '',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          miniCss.loader,
+          "css-loader",
+          "sass-loader",
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'images',
+        },
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new miniCss({
+      filename: 'style.css',
+    }),
+    new InjectManifest({
+      swSrc: './src/service-worker.js',
+      swDest: 'service-worker.js'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './src/manifest.json', to: '' },
+      ],
+    }),
+  ],
 };
